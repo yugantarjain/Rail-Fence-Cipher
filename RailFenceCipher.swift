20f31b00cd6c0838209cb20b79b09ee39a041bb1
Yugantar Jain <31164725+yugantarjain@users.noreply.github.com>
2020-10-15 20:35:53 +0530

    Create RailFenceCipher.swift

---
+++ RailFenceCipher.swift
@@ -0,0 +1,68 @@
+import Foundation
+
+func cipher(_ str: String, key: Int) -> String {
+    let str = str.filter { $0 != " " }
+    var addFactor = 1
+    var i = 0
+    var cipher = ""
+    var rails = [[Character]](repeating: [Character](), count: key)
+    
+    for char in str {
+        rails[i].append(char)
+        i += addFactor
+        if i == key - 1 || i == 0 {
+            addFactor *= -1
+        }
+    }
+    
+    for rail in rails {
+        for char in rail {
+            cipher.append(char)
+        }
+    }
+    
+    return cipher
+}
+
+func decipher(_ cipher: String, key: Int) -> String {
+    let n = cipher.count
+    let cipher = Array(cipher)
+    var addFactor = 1
+    var r = 0
+    var k = 0
+    var str = ""
+    var rails = [[Character]](repeating: [Character](repeating: " ", count: n), count: key)
+    
+    for c in 0 ..< n {
+        rails[r][c] = "*"
+        r += addFactor
+        if r == key - 1 || r == 0 {
+            addFactor *= -1
+        }
+    }
+    r = 0
+    
+    for r in 0 ..< key {
+        for c in 0 ..< n {
+            if rails[r][c] == "*" {
+                rails[r][c] = cipher[k]
+                k += 1
+            }
+        }
+    }
+    
+    r = 0
+    addFactor = 1
+    for c in 0 ..< n {
+        str.append(rails[r][c])
+        r += addFactor
+        if r == key - 1 || r == 0 {
+            addFactor *= -1
+        }
+    }
+    
+    return str
+}
+
+let cipherText = cipher("defend the east wall of the castle", key: 4)
+decipher(cipherText, key: 4)
